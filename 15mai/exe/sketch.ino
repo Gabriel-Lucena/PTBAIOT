96989ee478df61be4c9b7a65041e4b9392c6541e
Gabriel-Lucena <gabriel.lucena.cunha@hotmail.com>
2023-05-21 20:14:23 -0300

    fix: :bug: Botão controla o buzzer
    Encoder rotativo precisa ser pressionado novamente para o buzzer voltar a emitir

--- 15mai/exe/sketch.ino
+++ 15mai/exe/sketch.ino
@@ -10,6 +10,7 @@ bool botaoPressionado = false;
 void setup()
 {
   Serial.begin(115200);
+
   pinMode(pinoCLK, INPUT);
   pinMode(pinoDT, INPUT);
   pinMode(pinoBotao, INPUT_PULLUP);
@@ -28,45 +29,39 @@ void loop()
 
   if (newClk != lastClk)
   {
-    // Se alterou o valor de CLK
     lastClk = newClk;
+
     int dtValue = digitalRead(pinoDT);
+
     if (newClk == LOW && dtValue == HIGH)
     {
-      // Serial.println("Sentido horário ⏩");
-
       frequencia += 50;
     }
+
     if (newClk == LOW && dtValue == LOW)
     {
-      // Serial.println("Sentido anti-horário ⏪");
-
       frequencia -= 50;
     }
   }
 
   if (digitalRead(pinoBotao) == LOW)
   {
-    // Serial.println("Botão pressionado!");
-
     botaoPressionado = true;
+
+    noTone(pinoBuzzer);
+
+    delay(100);
 
     while (botaoPressionado == true)
     {
-
-      noTone(pinoBuzzer);
-
-      // Serial.println("Botão pressionado! 😮");
-
-      if (digitalRead((pinoBotao)) == LOW)
+      if (digitalRead(pinoBotao) == LOW)
       {
         break;
       }
     }
-    // Serial.println("Saiu!");
 
     botaoPressionado = false;
 
-    tone(pinoBuzzer, frequencia);
+    delay(100);
   }
 }
